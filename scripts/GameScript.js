461f377622febcf95be25ce96c1b0869ecdf19bb
Alex Glenn <157517894+pglenn1@users.noreply.github.com>
2024-05-04 15:58:54 -0500

    Update GameScript.js

--- scripts/GameScript.js
+++ scripts/GameScript.js
@@ -1,22 +1,70 @@
-    $(function() { // DOM ready
-      // If a link has a dropdown, add sub menu toggle.
-      $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        // Close one dropdown when selecting another
-        $('.nav-dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
-      });
-      // Clicking away from dropdown will remove the dropdown class
-      $('html').click(function() {
-        $('.nav-dropdown').hide();
-      });
-      // Toggle open and close nav styles on click
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      // Hamburger to X toggle
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-    }); // end DOM ready
+$(function() {
   
+
+const games = [
+  {
+    title: "007",
+    link: "James Bond", 
+    author: "Games Bond"
+  },
+  {
+    title: "Name Game",
+    link: "https://zanemoon13.github.io/lab-echo/index.html", 
+    author: "Zane Mooney"
+  },
+  {
+    title: "Macey's Game",
+    link: "https://maceyraejones.github.io/cautious-couscous/",
+    author: "Macey Jones"
+    },
+    {
+title: "EchoGame",
+link: "https://brandoncoplen.github.io/EchoLab1/",
+author: "Brandon Coplen"
+},
+{
+  title: "NameGame",
+  link: "https://nkerstiens.github.io/Echo-Lab-376/",
+  author: "Noah Kerstiens"
+  },
+  {
+    title: "game4happiness",
+    link: "https://vanpham2000.github.io/game4happiness/",
+    author: "Van Pham"
+    },
+    {
+      title: "namePage",
+      link: "https://2fortdev.github.io/Echo-lab/",
+      author: "Sam Aaron"
+      }
+];
+// iterate over the JSON array
+$.each(games, function (index, item) {
+  console.log("Game Title: " + item.title);
+  console.log("Game URL: " + item.link);
+  console.log("Game Author: " + item.author);
+
+  // create a brand new HTML element JUST with code
+
+var el = `<div class='card text-center mx-auto bg-dark blue' style="width: 200px"> \
+  <button class='cardOpen2 btn btn-outline-info'> ${ item.title } </button> \
+   <div class='cardHide'> \
+       <br> \
+       <a href="${ item.link }" target="_blank" class="link-info" >GAME LINK</a> \
+       <p class="text-white">Create by: ${ item.author } </p> \
+       <br> \
+       <button class='btn btn-outline-info cardClose2'>Close</button> \
+   </div>
+ </div>`; 
+
+ console.log(el); 
+
+
+$('#firstCard').append(el);
+
+$('.nav-dropdown').append($(`<li><a href="#!"> ${item.author} </a></li>`));
+
+});
+
+
+});
